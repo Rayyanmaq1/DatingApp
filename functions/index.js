608d26805d8769b1f77bf49194361d9d907118af
Rayyan Maqsood <rayyanmaq1@gmail.com>
2022-02-24 20:42:10 +0500

    funtion

--- functions/index.js
+++ functions/index.js
@@ -10,10 +10,41 @@ var fcm=admin.messaging();
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.connectVideocall= functions.firestore.document('SearchVideoCall/{id}').onCreate(async(snap,context)=>{
-    size = snap.docs.length;
-    console.log("Length",size);
-    if(size >=2){
-        console.log("FOUNT TWO");
+   const newValue =await snap.data();
 
+  const genderSearch =await newValue.SelectedGender;
+    console.log('selected',genderSearch);
+    if(genderSearch == 1){
+        await fb.collection('SearchVideoCall').where('SelectedGender', '==' ,1).orderBy('timeStamp','desc').get().then(snapshot => {
+        if(snapshot.docs.length>2){
+            snapshot.docs.map((e)=>{
+                if(e.Uid != newValue.Uid){
+                    let uniqueID = (Math.random() + 1).toString(36).substring(7);
+                     fb.collection('${ConnectedVideocall}').add({ 
+
+                      }).then(ref => {
+                    console.log('Added document with ID: ', ref.id)
+                    });
+
+
+                    break;
+                }
+            });
+        }
+        
+    });
     }
+   else if(genderSearch == 2){
+        await fb.collection('SearchVideoCall').where('SelectedGender', '==' ,2).orderBy('timeStamp','desc').get().then(snapshot => {
+        size = snapshot.docs.length;
+        console.log("MALE SIZE" , size);
+    });
+   }
+   else if(genderSearch == 3){
+        await fb.collection('SearchVideoCall').where('SelectedGender', '==' ,3).orderBy('timeStamp','desc').get().then(snapshot => {
+        size = snapshot.docs.length;
+        console.log("FEMALE SIZE" , size);
+    });
+ }
+   
 });
